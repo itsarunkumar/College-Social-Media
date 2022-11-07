ed3213faebf0fcb8ece76f50543868307f60eeb3
itsarunkumar <003arunkumark@gmail.com>
2022-11-07 20:06:27 +0530

    code added

---
+++ client/src/Assets/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+function logo() {
+  return (
+    <img
+      className="logo"
+      src="../../images/tpgit.png"
+      alt="logo"
+      width="300px"
+    />
+  );
+}
+export default logo;
